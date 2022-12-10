7d5ecae83da2ae293e0c7691080e893d84c2de80
yury_klimchuk <hydro.yura@gmail.com>
2022-12-10 22:05:49 +0300

    add frontendweb mircoservice

---
+++ frontendweb/src/main/java/com/hydroyura/productionmanager/frontendweb/FrontendwebApplication.java
@@ -0,0 +1,13 @@
+package com.hydroyura.productionmanager.frontendweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendwebApplication.class, args);
+	}
+
+}
---
+++ frontendweb/src/test/java/com/hydroyura/productionmanager/frontendweb/FrontendwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hydroyura.productionmanager.frontendweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
